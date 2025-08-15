fc161a435fd242931d0016bee9b9c79efbef9ac2
revanth.ayodhya <revanth@nuevesolutions.in>
2025-08-15 10:27:42 +0530

    updated logic

--- Leetcode/Arrays/1752.py
+++ Leetcode/Arrays/1752.py
@@ -14,3 +14,22 @@ class Solution(object):
             if count==n:
                 return True
         return False
+
+
+
+
+class Solution(object):
+    def check(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n=len(nums)
+        count=0
+        for i in range(len(nums)):
+            if(nums[i])>nums[(i+1)%n]:
+                count+=1
+                if count>1:
+                    return False
+        return True
+        
