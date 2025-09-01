8afbb7cbb7200ac4897560ac38f92ca86677c14d
revanth.ayodhya <revanth@nuevesolutions.in>
2025-09-01 17:35:28 +0530

    practice

---
+++ Leetcode/Arrays/162.py
@@ -0,0 +1,17 @@
+# Find Peak Element
+
+
+from typing import List
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l,r=0,len(nums)-1
+        while l<=r:
+            m=l+((r-l)//2)
+            if m<len(nums)-1 and nums[m]<nums[m+1]:
+                l=m+1
+            elif m>0 and nums[m-1]>nums[m]:
+                r=m-1
+            else:
+                return m
+        
