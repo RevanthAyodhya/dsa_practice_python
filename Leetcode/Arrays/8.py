9f919c08677589ba74c5734ae3324b7da2fd0c8a
revanth.ayodhya <revanth@nuevesolutions.in>
2025-10-04 10:25:44 +0530

    practice

---
+++ Leetcode/Arrays/8.py
@@ -0,0 +1,31 @@
+
+# String to Integer (atoi)
+
+
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s=s.strip()
+        if not s:
+            return 0
+        sign=1
+        res=0
+        if s[0]=='-' or s[0]=='+':
+            if s[0]=='-':
+                sign=-1
+            s=s[1:]
+        for char in s:
+            if not char.isdigit():
+                break
+            else:
+                res=res*10 + int(char)
+        
+        res=res*sign
+        if res<-2**31:
+            return -2**31
+        if res>2**31-1:
+            return 2**31-1
+
+
+        return res
+        
