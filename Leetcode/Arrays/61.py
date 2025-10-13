5ff608fc745e8c88a83190e3b31aaef7af6094db
revanth.ayodhya <revanth@nuevesolutions.in>
2025-10-13 22:21:58 +0530

    practice

---
+++ Leetcode/Arrays/61.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+  
+        length, tail = 1, head
+        while tail.next:
+            tail = tail.next
+            length += 1
+        
+     
+        k %= length
+        if k == 0:
+            return head
+        
+       
+        curr = head
+        for i in range(length - k - 1):
+            curr = curr.next
+        
+
+        new_head = curr.next
+        curr.next = None
+        tail.next = head
+        
+        return new_head
